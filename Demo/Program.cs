7633a93e5f03f7c7037097add871413eafd5cd87
Khun <khun.ly@bstorm.be>
2024-05-22 10:21:57 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,75 @@
+﻿#region Variables
+//// déclaration d'une variable
+//int number;
+
+//// affection d'une variable 
+//number = 42;
+
+//// déclarer et affecter une variable
+//string prenom = "Khun";
+//char lettre = 'a';
+
+//// déclaration multiple
+//int variable1, 
+//    variable2, 
+//    variable3; 
+#endregion
+
+#region Output
+// cw -> tab
+////Console.WriteLine("Hello\nworld\n!");
+////Console.WriteLine(@"Hello
+////world
+////!");
+
+//Console.WriteLine("Hello " + prenom + " !");
+//Console.WriteLine($@"Hello
+//{prenom}
+//!");
+#endregion
+
+#region Correction Swap variables
+//int a = 5,
+//    b = 7;
+
+//Console.WriteLine($"a: {a}; b: {b}");
+
+////int temp = a;
+////a = b;
+////b = temp;
+//(a, b) = (b, a);
+
+//Console.WriteLine($"a: {a}; b: {b}");
+#endregion
+
+#region Input
+//Console.WriteLine("Entrer votre prénom");
+//// input lire la ligne entrée
+//string prenom = Console.ReadLine();
+//Console.WriteLine($"Bonjour Monsieur {prenom} !!");
+
+//Console.WriteLine("Entrer un nombre");
+//string input = Console.ReadLine();
+
+//// conversion d'un chaine en entier
+//int nombre = int.Parse(input);
+//Console.WriteLine($"Ce nombre au carré = {nombre * nombre}");
+
+// conversion d'une chaine en date
+//Console.WriteLine("Entrer votre date de naissance");
+
+//string input = Console.ReadLine();
+//DateTime date = DateTime.Parse(input);
+//Console.WriteLine($"Vous êtes nés, il y {(DateTime.Now - date).TotalDays} jours");
+#endregion
+
+
+#region Opérateurs
+int nb = 5;
+
+Console.WriteLine($"42 + 42 = {42 + 42}");
+Console.WriteLine($"42 - 42 = {42 - 42}");
+Console.WriteLine($"42 * 42 = {42 * 42}");
+Console.WriteLine($"42 / 42 = {42 / (double)nb}");
+#endregion
+
