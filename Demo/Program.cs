acf765b3fc92d35b8c4bf27988726dcbab86c2d4
Khun <khun.ly@bstorm.be>
2024-05-22 11:48:38 +0200

    operateurs logique + stuctures conditionnelles

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -63,13 +63,156 @@
 //Console.WriteLine($"Vous êtes nés, il y {(DateTime.Now - date).TotalDays} jours");
 #endregion
 
-
 #region Opérateurs
-int nb = 5;
+//int nb1 = 5;
+//int nb2 = 42;
 
-Console.WriteLine($"42 + 42 = {42 + 42}");
-Console.WriteLine($"42 - 42 = {42 - 42}");
-Console.WriteLine($"42 * 42 = {42 * 42}");
-Console.WriteLine($"42 / 42 = {42 / (double)nb}");
+//Console.WriteLine($"{nb2} + {nb1} = {nb2 + nb1}");
+//Console.WriteLine($"{nb2} - {nb1} = {nb2 - nb1}");
+//Console.WriteLine($"{nb2} * {nb1} = {nb2 * nb1}");
+
+//// division entière
+//Console.WriteLine($"{nb2} / {nb1} = {nb2 / nb1}");
+
+//// division
+//Console.WriteLine($"{nb2} / {nb1} = {nb2 / (double)nb1}");
+
+//// modulo
+//Console.WriteLine($"{nb2} % {nb2} = {nb2 % nb1}");
+
+//// concaténation
+
+//string prenom = "Khun";
+//Console.WriteLine("Hello " + prenom + "!");
+
 #endregion
 
+#region Correction Convertisseur secondes
+//int secondesTotales, 
+//    secondes, 
+//    minutes, 
+//    heures, 
+//    jours;
+
+//Console.WriteLine("Entrer le nombre de secondes pour la conversion");
+//secondesTotales = int.Parse(Console.ReadLine());
+
+//secondes = secondesTotales % 60;
+//minutes = secondesTotales / 60 % 60;
+//heures = secondesTotales / 3600 % 24;
+//jours = secondesTotales / 864000;
+
+//Console.WriteLine($"jours : {jours}\nheures : {heures}\nminutes : {minutes}\nsecondes : {secondes}");
+#endregion
+
+# region bool
+//bool flag = false;
+
+//int age = 42;
+//bool estMajeur = age >= 18;
+// égal → == 
+// différent → !=
+// plus petit → <
+// plus petit ou égal → <=
+// plus grand → >
+// plus grand ou égal → >=
+
+
+// XOR
+// Console.WriteLine(true ^ true);
+
+#endregion
+
+#region Structures contionnelles
+//bool estAutorise = true;
+//// (cond à vérifier)
+//if(estAutorise)
+//{
+//    // ensemble des instructions qui seront éxecutées si vrai
+//    Console.WriteLine("Est affiché si autorisé est vrai");
+//}
+//else
+//{
+//    // ensemble des instructions qui seront éxecutées sinon
+//    Console.WriteLine("Est affiché si pas autorisé");
+//}
+
+//int age = 42;
+//if(age < 12)
+//{
+//    Console.WriteLine("Enfant");
+//}
+//else if(age < 18)
+//{
+//    Console.WriteLine("Ado");
+//}
+//else
+//{
+//    Console.WriteLine("Adulte");
+//}
+
+//int jourSemaine = 5;
+
+//switch(jourSemaine)
+//{
+//    case 1:
+//        Console.WriteLine("Lundi");
+//        break;
+//    case 2:
+//        Console.WriteLine("Mardi");
+//        break;
+//    case 3:
+//        Console.WriteLine("Mercredi");
+//        break;
+//    case 4:
+//        Console.WriteLine("Jeudi");
+//        break;
+//    case 5:
+//        Console.WriteLine("Vendredi");
+//        break;
+//    case 6:
+//        Console.WriteLine("Samedi");
+//        break;
+//    case 7:
+//        Console.WriteLine("Dimanche");
+//        break;
+//    default:
+//        Console.WriteLine("Ce n'est un jour de la semaine !!");
+//        break;
+//}
+
+//int age = 14;
+
+// Opérateur ternaire
+//string categorie = age < 18 ? "enfant" : "adulte";
+// <=>
+//string categorie;
+//if(age < 18)
+//{
+//    categorie = "enfant";
+//}
+//else
+//{
+//    categorie = "adulte";
+//}
+
+//int? promo = 25;
+//decimal prix = 14;
+//decimal prixPromo;
+
+//prixPromo = prix - (prix * (promo ?? 0) / 100);
+//Console.WriteLine(prixPromo);
+
+//int jours = 42;
+////string j = jours == 1 ? "Lundi" : jours == 2 ? "Mardi" : jours == 3 ? "Mercredi": "Jeudi";//...
+//string j = jours switch
+//{
+//    1 => "Lundi",
+//    2 => "Mardi",
+//    3 => "Mercredi",
+//    4 => "Jeudi",
+//    _ => "Jour inconnu"
+//};
+//Console.WriteLine(j);
+
+#endregion
